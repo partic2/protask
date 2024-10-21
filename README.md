0788688aa3062ada16be69fffeb71643fa818af2
partic2 <1596067968@qq.com>
2024-10-21 13:42:14 +0800

    2.0.0

--- README.md
+++ README.md
@@ -1,15 +1,53 @@
 
 
 
-"**protask**" is a task library to provide task manager.
+"**protask**" is a library to manage tasks.
 
-By hook the Promise method, You can "abort" a task or asynchronous function without manual "throwIfAborted".
-Also this library provide task local variable support.
-
-Note: "Promise" Created in asynchronous function will inherit the current task, But not necessarily for "Promise" created in callback.
+You can abort a task, or access "task local" (like thread local in Java) like below
 
 **Not well tested, use at your own risk.**
 
+
+### Usage
+
+``` typescript
+import { Task } from 'protask';
+
+function sleep<T>(milliSeconds: number, arg?: T): Promise<T> {
+    return new Promise(function (resolve, reject) {
+        setTimeout(resolve, milliSeconds, arg);
+    });
+}
+
+function *printTaskLocal() {
+    yield* Task.yieldWrap(sleep(100));
+    console.info(Task.locals());
+}
+
+let task1=new Task(function *(){
+    try {
+        for (let i = 0; i < 100; i++) {
+            Task.locals()!.count = i;
+            console.log('task 1 running');
+            yield* printTaskLocal();
+            yield* Task.yieldWrap(sleep(1000));
+        }
+        console.log('Task 1 resolve');
+    } catch (e: any) {
+        console.log('Task 1 error');
+        console.info(e.toString());
+    } finally {
+        console.log('Task 1 finally');
+    }
+},'test task 1',).run();
+
+let task2=new Task(function*(){
+    yield* Task.yieldWrap(sleep(3000));
+    console.info('abort task 1');
+    task1.abort();
+}).run();
+
+```
 
 ### NPM Install 
 
@@ -17,43 +55,7 @@ Note: "Promise" Created in asynchronous function will inherit the current task, 
 npm i protask
 ```
 
-### Usage
+### Note
+protask 1.x hook the Promise to implement Task inheritation.But it work incorrectly in many case.
 
-``` typescript
-
-import {task} from 'protask'
-
-function sleep<T>(milliSeconds: number, arg?: T): Promise<T> {
-    return new Promise(function (resolve, reject) {
-        setTimeout(resolve, milliSeconds, arg)
-    });
-}
-
-async function printTaskLocal(){
-    await sleep(100);
-    console.info(task.locals());
-}
-
-task.create('test task 1',async ()=>{
-    try{
-        for(let i=0;i<100;i++){
-            task.locals()!.count=i;
-            await printTaskLocal();
-            await sleep(1000)
-        }
-    }catch(e:any){
-        console.info(e.toString());
-    }
-});
-
-task.create('test task 2',(async ()=>{
-    await sleep(3000);
-    console.info('abort task 1')
-    task.abort('test task 1');
-}));
-
-```
-
-
-### Known Issue
-'task' may pollute Promise create outside task scope.
+So 2.x use generator and custom scheduler instead. But it's not compatible with native async/await mechanism. Maybe a source convert like babel should also provide.
--- src/index.ts
+++ src/index.ts
@@ -1,76 +1,69 @@
 
-class HookedPromise<T> extends Promise<T>{
-    __taskId:string='';
-    static task={
-        status:{} as {[taskId:string]:{
-            locals:{[key:string]:any},
-            abortSignal:AbortSignal,
-            abortController:AbortController;
-        }},
-        currentTask:'',
-        getAbortSignal(taskId?:string){
-            taskId=taskId??this.currentTask;
-            if(taskId in this.status){
-                return this.status[taskId].abortSignal;
+
+export class Task<T>{
+    static currentTask:Task<any>|null=null;
+    static locals(){
+        return Task.currentTask?.locals();
+    }
+    static getAbortSignal(){
+        return Task.currentTask?.getAbortSignal();
+    }
+    static *yieldWrap<T2>(p:Promise<T2>){
+        return (yield p) as T2;
+    }
+    constructor(taskMain:Generator<Promise<any>,T,any>|(()=>Generator<Promise<any>,T,any>),
+                public name?:string){
+        this.__iter=(typeof taskMain==='function')?taskMain():taskMain;
+        let resolver:Partial<typeof this.__resolver>=[undefined,undefined,undefined];
+        resolver[0]=new Promise((resolve,reject)=>{
+            resolver![1]=resolve;
+            resolver![2]=reject;
+        });
+        this.__resolver=resolver as any;
+    }
+    __resolver?:[Promise<T>,((value: T) => void),((reason?: any) => void)]
+    __iter?:Generator<Promise<any>>;
+    __locals={};
+    __abortController=new AbortController();
+    __step(tNext:any,error:any){
+        Task.currentTask=this;
+        try{
+            if(this.__abortController.signal.aborted){
+                this.__iter!.throw(this.__abortController.signal.reason);
             }
-        },
-        abort(taskId:string,reason?:any){
-            if(taskId in this.status){
-                this.status[taskId].abortController.abort(reason);
+            if(error!=undefined){
+                this.__iter!.throw(error);
             }
-        },
-        create(taskId:string,taskMain:()=>Promise<void>){
-            if(taskId in this.status){
-                throw new Error('task is running.');
+            let yieldResult=this.__iter!.next(tNext);
+            if(!yieldResult.done){
+                yieldResult.value.then(
+                    r=>this.__step(r,undefined),
+                    reason=>this.__step(undefined,reason)
+                );
+            }else{
+                Task.currentTask=null;
+                this.__resolver![1](yieldResult.value);
             }
-            let abortController=new AbortController();
-            this.status[taskId]={locals:{},abortSignal:abortController.signal,abortController};
-            this.currentTask=taskId;
-            const cleanup=()=>{
-                delete this.status[taskId];
-            }
-            taskMain().then(cleanup).catch(cleanup);
-            this.currentTask='';
-        },
-        locals(taskId?:string){
-            taskId=taskId??this.currentTask;
-            if(taskId in this.status){
-                return this.status[taskId].locals
-            }
+        }catch(e){
+            this.__resolver![2](e);
+        }finally{
+            Task.currentTask=null;
         }
     }
-    constructor(arg0:any){
-        super(arg0);
-        this.__taskId=task.currentTask;
+    run(){
+        this.__step(undefined,undefined);
+        return this;
     }
-    then<TResult1 = T, TResult2 = never>(onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | null | undefined, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | null | undefined): Promise<TResult1 | TResult2> {
-        return super.then((value: T) => {
-            task.currentTask=this.__taskId;
-            try{
-                task.getAbortSignal()?.throwIfAborted();
-                if(typeof onfulfilled==='function'){
-                    return onfulfilled(value);
-                }else{
-                    return value;
-                }
-            }catch(e:any){
-                if(typeof onrejected==='function'){
-                    return onrejected(e);
-                }
-            }
-        },(reason: any) => {
-            task.currentTask=this.__taskId;
-            if(typeof onrejected==='function'){
-                return onrejected(reason);
-            }else{
-                throw reason;
-            }
-        }) as any;
+    abort(reason?:any){
+        this.__abortController.abort(reason??new Error('aborted'));
+    }
+    getAbortSignal(){
+        return this.__abortController.signal;
+    }
+    locals():Record<string,any>{
+        return this.__locals;
+    }
+    then<TResult1 = T, TResult2 = never>(onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null): Promise<TResult1 | TResult2>{
+        return this.__resolver![0].then(onfulfilled,onrejected);
     }
 }
-if(!('task' in Promise)){
-    (globalThis as any).__NativePromise=Promise;
-    (globalThis as any).Promise=HookedPromise;
-}
-
-export let task=(Promise as any).task as typeof HookedPromise.task;
